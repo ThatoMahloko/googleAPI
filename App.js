a37fc86fbfcfc1850903a80fe56d9adc094de9d6
ThatoMahloko <thato732mahloko@gmail.com>
2021-09-18 20:25:42 +0200

    get location works

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import useGeoLocation from './API/useGeoLocation';
 export default function App() {
+  const location = useGeoLocation();
+  const latititude =location.coordinates.lat;
+  const longitude = location.coordinates.lng;
+  console.log(latititude, longitude);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      
+      {location.loaded ? JSON.stringify(location) : "location data not available"}
     </View>
   );
 }
